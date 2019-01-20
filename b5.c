640aa21cc5d81b70eb214399eab7f6a4dd4a1b3c
prasanthkailsh <46813935+prasanthkailsh@users.noreply.github.com>
2019-01-20 14:05:32 +0530

    Create b5.c

---
+++ b5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num1,num2,num3;
+printf("enter the value num1,num2,num3");
+scanf("%d%d%d",&num1,&num2,&num3);
+if((num1>num2)&&(num1>num3))
+printf("num1 is greater");
+else if((num2>num3)&&(num2>num1))
+printf("num2 is greater");
+else
+printf("num3 is greater");
+}
