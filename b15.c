3a53733a3ec9a81f0172fec1956445ff8ac6bf54
prasanthkailsh <46813935+prasanthkailsh@users.noreply.github.com>
2019-03-19 15:44:34 +0530

    Update b15.c

--- b15.c
+++ b15.c
@@ -2,9 +2,9 @@
 void main()
 {
 int n1,n2,result,i;
-printf("Enter the lowest limit :");
+printf("Enter the 1st intervel :");
 scanf("%d",&n1);
-printf("Enter the highest limit :");
+printf("Enter the last intervel :");
 scanf("%d",&n2);
 for(i=n1+1;i<n2;i++)
 {
